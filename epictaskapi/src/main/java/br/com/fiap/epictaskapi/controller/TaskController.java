e52ad620ed93c852c97e626e3909e975c2120b5e
Joao <joaocarloslima@me.com>
2022-08-09 20:58:18 -0300

    Spring Data

--- epictaskapi/src/main/java/br/com/fiap/epictaskapi/controller/TaskController.java
+++ epictaskapi/src/main/java/br/com/fiap/epictaskapi/controller/TaskController.java
@@ -2,8 +2,14 @@ package br.com.fiap.epictaskapi.controller;
 
 import java.util.List;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.fiap.epictaskapi.model.Task;
@@ -18,6 +24,12 @@ public class TaskController {
     @GetMapping("/api/task")
     public List<Task> index(){
         return service.listAll();
+    }
+
+    @PostMapping("/api/task")
+    public ResponseEntity<Task> create(@RequestBody @Valid Task task){
+        service.save(task);
+        return ResponseEntity.status(HttpStatus.CREATED).body(task);
     }
 
 }
